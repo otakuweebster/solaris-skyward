f0b9acc3bd37c724a7edc4f1a66f0093ef713300
otakuweebster <gabrielaudbeltran23@gmail.com>
2023-11-23 23:17:49 -0600

    Made a react placeholder

---
+++ frontend/solaris-skyward/src/LogInScreen.js
@@ -0,0 +1,22 @@
+
+const tempStyle = {
+    "display": "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "text-align": "center",
+    "min-height": "100vh",
+}
+
+
+function LogInScreen()
+{
+    return (
+        <div style={tempStyle}>
+            <p>
+                <h1>Solaris Skyward: The Digital Guardian</h1> <br></br> Coming not soon! :)
+            </p>
+        </div>
+    )
+}
+
+export default LogInScreen;
